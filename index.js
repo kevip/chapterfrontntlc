ab09ec9fafe57c664e8dae9df7b874eba32da483
kevip <castaneda.kevin114@gmail.com>
2023-05-28 21:22:47 -0500

    feat: display variables

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const server = express();
 const port = process.env.PORT || 3000;
 
-server.get('/', (req, resp) => resp.send('Hola mundo'))
+server.get('/', (req, resp) => resp.send(`
+    Mi puerto es: ${port} <br>
+    Mis variables de entorno: ${JSON.stringify(process.env)}
+`))
 
 server.listen(port, () => console.log('server is running on.... ', port));
