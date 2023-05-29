d3a90c0b5bf3632d7b5564ce405430518523eed9
kevip <castaneda.kevin114@gmail.com>
2023-05-28 21:11:40 -0500

    feat: add server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log(process.env);
+const express = require('express');
+const server = express();
+const port = process.env.PORT || 3000;
+
+server.get('/', (req, resp) => resp.send('Hola mundo'))
+
+server.listen(port, () => console.log('server is running on.... ', port));
