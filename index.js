f978d6c57f065075c133a630d92e94a5f4c00c46
kevip <castaneda.kevin114@gmail.com>
2023-06-01 21:44:23 -0500

    feat: new change

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const server = express();
 const port = process.env.PORT || 3000;
 
-server.get('/', (req, resp) => resp.send(`
-    Nuevos cambios!<br>
-    Mi puerto es: ${port} <br>
-    Mis variables de entorno: ${JSON.stringify(process.env)}
-`))
+server.get('/', (req, resp) => {
+    console.log(req);
+
+    return resp.send(`
+        Nuevos cambios!<br>
+        Mi puerto es: ${port} <br>
+        Mis variables de entorno: ${JSON.stringify(process.env)}
+    `);
+});
+server.get('/error', (req, resp) => {
+    throw new Error('error de prueba');
+    
+})
 
 server.listen(port, () => console.log('server is running on.... ', port));
